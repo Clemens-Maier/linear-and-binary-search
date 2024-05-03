adb1089be7dd7491e11fda3d8bf2fc5dbc1beb63
C. Maier <168710895+clemens-maier@users.noreply.github.com>
2024-05-03 08:59:43 +0200

    Add SearchAlgorithms class and methods for linear and binary search

---
+++ src/main/java/de/jkgweil/informatik/SearchAlgorithms.java
@@ -0,0 +1,37 @@
+package de.jkgweil.informatik;
+
+/**
+ * The class SearchAlgorithms implements several different search algorithms.
+ * This class is for teaching purposes, the implementations are missing on purpose. If all tests
+ * are passed, the implementation ist correct.
+ *
+ * @author C. Maier
+ *
+ */
+public class SearchAlgorithms
+{
+    /**
+     * The method linearSearch searches a given array for the given value using linear search.
+     *
+     * @param array: the given array to search through
+     * @param searchValue: the given value to search for
+     * @return the position within the array of the value, if value not present return -1
+     */
+    public static int linearSearch(int[] array, int searchValue) {
+        //TODO: implement
+        return -1;
+    }
+
+    /**
+     * The method binarySearch searches a given array for the given value using binary search.
+     *
+     * @param array: the given array to search through
+     * @param searchValue: the given value to search for
+     * @return the position within the array of the value, if value not present return -1
+     */
+    public static int binarySearch(int[] array, int searchValue) {
+        //TODO: implement
+        return -1;
+    }
+
+}
---
+++ src/test/java/de/jkgweil/informatik/SearchAlgorithmsTest.java
@@ -0,0 +1,12 @@
+package de.jkgweil.informatik;
+
+import junit.framework.TestCase;
+
+public class SearchAlgorithmsTest extends TestCase {
+
+    public void testLinearSearch() {
+    }
+
+    public void testBinarySearch() {
+    }
+}
